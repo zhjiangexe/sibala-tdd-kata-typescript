f97dc8041927dbe123bcdb7ed7d6faba8d1c3a3d
jiang <jiang.jiang@eton.tw>
2022-03-12 22:50:22 +0800

    [feat] pass: winnerOutput real code

--- src/Game.ts
+++ src/Game.ts
@@ -1,12 +1,24 @@
 import {Parser} from "./Parser"
+import {Dice} from "./Dice"
+
 export class Game {
   showResult(input: string): string {
-    // todo: hard code
-    // "Black: 5 5 5 5 White: 3 3 3 3"
     const parser = new Parser()
     const players = parser.parse(input)
     const winnerPlayer = players[0].name
-    const winnerOutput = "5"
+    const winnerOutput = this.countMapNum(players[0].dices)[4]
     return `${winnerPlayer} win. - with all of a kind: ${winnerOutput}`
+  }
+
+  private countMapNum(dices: Dice[]) {
+    const numMapCount: Record<string, number> = {}
+    for (const item of dices) {
+      numMapCount[item.num] = numMapCount[item.num] ? numMapCount[item.num] + 1 : 1
+    }
+    const countMapNum: Record<number, string> = {}
+    for (let num in numMapCount) {
+      countMapNum[numMapCount[num]] = num
+    }
+    return countMapNum
   }
 }
