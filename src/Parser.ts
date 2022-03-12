b29fad1b77d441c19b3a5eb1b2d676eb6f5bdd6b
jiang <jiang.jiang@eton.tw>
2022-03-12 15:40:36 +0800

    implement pass: parse player1 dices

--- src/Parser.ts
+++ src/Parser.ts
@@ -3,17 +3,13 @@ import {Dice} from "./Dice"
 
 export class Parser {
   parse(input: string): Player[] {
+    // Black: 5 5 5 5  White: 3 3 3 3
     const playerSections = input.split("  ")
     const player1Name = playerSections[0].split(":")[0]
     const player2Name = playerSections[1].split(":")[0]
+    const player1Dices = playerSections[0].split(":")[1].trim().split(" ").map(dice => new Dice(dice))
     return [
-      new Player(player1Name,
-        [
-          new Dice("5"),
-          new Dice("5"),
-          new Dice("5"),
-          new Dice("5")
-        ]),
+      new Player(player1Name, player1Dices),
       new Player(player2Name, [])
     ]
   }
