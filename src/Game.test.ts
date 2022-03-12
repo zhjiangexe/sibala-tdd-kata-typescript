689a7f4d13b8922581528617742ed943ec6a5442
jiang <jiang.jiang@eton.tw>
2022-03-12 22:58:15 +0800

    [refactor] refactor test, init SUT on top of code

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,9 +1,15 @@
 import {Game} from "./Game"
 
 describe("game", () => {
+  const game = new Game()
+
+  const resultShouldBe = (input: string, result: string) => {
+    const showResult: string = game.showResult(input)
+    expect(showResult).toEqual(result)
+  }
+
   test("both all of a kind", () => {
-    const game = new Game()
-    const showResult: string = game.showResult("Black: 5 5 5 5  White: 3 3 3 3")
-    expect(showResult).toEqual("Black win. - with all of a kind: 5")
+    resultShouldBe("Black: 5 5 5 5  White: 3 3 3 3",
+      "Black win. - with all of a kind: 5")
   })
 })
