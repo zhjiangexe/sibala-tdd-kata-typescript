4f7f12d797fddcb8a367644bf875dad2382c05f1
jiang <jiang.jiang@eton.tw>
2022-03-12 15:13:34 +0800

    implement: pass player name Black

--- src/Parser.ts
+++ src/Parser.ts
@@ -2,8 +2,10 @@ import {Player} from "./Player"
 
 export class Parser {
   parse(input: string): Player[] {
+    const playerSections = input.split("  ")
+    const player1Name = playerSections[0].split(":")[0]
     return [
-      new Player("Black"),
+      new Player(player1Name),
       new Player("White")
     ]
   }
