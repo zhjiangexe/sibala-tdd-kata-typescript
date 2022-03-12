f13b54a709582d6212efc2614dd4088f93bc8947
jiang <jiang.jiang@eton.tw>
2022-03-12 14:08:54 +0800

    add test case: both all of a kind and hard code

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,5 +1,14 @@
+class Game {
+  showResult(input: string): string {
+    // todo: hard code
+    return "Black win. - with all of a kind: 5"
+  }
+}
+
 describe("game", () => {
   test("both all of a kind", () => {
-    expect("test").toEqual("NOT TEST")
+    const game = new Game()
+    const showResult: string = game.showResult("Black: 5 5 5 5 White: 3 3 3 3")
+    expect(showResult).toEqual("Black win. - with all of a kind: 5")
   })
 })
